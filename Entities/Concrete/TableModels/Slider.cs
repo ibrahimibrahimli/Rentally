5333e2d686d09282f8515585a67250eb4432159d
Ibrahim Ibrahimli <dev.ibrahimlii@gmail.com>
2024-05-10 16:42:59 +0400

    Added Entites

---
+++ Entities/Concrete/TableModels/Slider.cs
@@ -0,0 +1,17 @@
+﻿using Core.Entities.Abstract;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Entities.Concrete.TableModels
+{
+    public class Slider : BaseEntity
+    {
+        public string Title { get; set; }
+        public string Description { get; set; }
+        public string ImageUrl { get; set; }
+
+    }
+}
