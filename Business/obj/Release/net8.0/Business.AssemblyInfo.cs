9dda0cf52909c0107c9415d06bb170101136f77c
unknown <dev.ibrahimlii@gmail.com>
2024-07-22 23:37:27 +0400

    Live Chat added all apges

--- Business/obj/Release/net8.0/Business.AssemblyInfo.cs
+++ Business/obj/Release/net8.0/Business.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Business")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Release")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+fc4bf3a1c6baad278956a45530a46129efc17adf")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acd4bd0c87581addb94215448064c5c6f299704a")]
 [assembly: System.Reflection.AssemblyProductAttribute("Business")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Business")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
--- DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
+++ DataAccess/obj/Debug/net8.0/DataAccess.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d9e517ba5a950980c731e26bc5aba49c377dbc34")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+acd4bd0c87581addb94215448064c5c6f299704a")]
 [assembly: System.Reflection.AssemblyProductAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DataAccess")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
